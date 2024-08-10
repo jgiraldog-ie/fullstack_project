5a69ff6bc110b5d40d69c12243bd9aac73271a6b
Camilo <jgiraldog.ie@gmail.com>
2024-08-10 11:19:28 -0400

    Lint corrections

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -6,17 +6,20 @@ class CarModelInline(admin.TabularInline):
     model = CarModel
     extra = 1
 
+
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'year', 'transmission', 'car_make')
     search_fields = ('name', 'car_make__name')
     list_filter = ('type', 'year', 'transmission', 'car_make')
     ordering = ('-year',)
 
+
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
-    list_display = ('name','description', 'country')
-    search_fields = ('name',)
+    list_display = ('name', 'description', 'country')
+    search_fields = ('name')
     list_filter = ('country',)
+
 
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -16,7 +16,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarMake',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID'
+                    )
+                ),
                 ('name', models.CharField(max_length=100)),
                 ('description', models.TextField()),
                 ('country', models.CharField(max_length=60)),
@@ -25,12 +33,68 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarModel',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID'
+                    )
+                ),
                 ('name', models.CharField(max_length=50)),
-                ('type', models.CharField(choices=[('SEDAN', 'Sedan'), ('COUPE', 'Coupe'), ('HATCHBACK', 'Hatchback'), ('SUV', 'SUV'), ('CROSSOVER', 'Crossover'), ('CONVERTIBLE', 'Convertible'), ('WAGON', 'Wagon'), ('PICKUP', 'Pickup Truck'), ('MINIVAN', 'Minivan'), ('SPORTS_CAR', 'Sports Car'), ('LUXURY_CAR', 'Luxury Car'), ('ELECTRIC', 'Electric Vehicle'), ('HYBRID', 'Hybrid Vehicle'), ('COMPACT', 'Compact Car')], default='SUV', max_length=12)),
-                ('year', models.IntegerField(default=2023, validators=[django.core.validators.MaxValueValidator(2023), django.core.validators.MinValueValidator(2015)])),
-                ('transmission', models.CharField(choices=[('MANUAL', 'Manual'), ('AUTOMATIC', 'Automatic'), ('CVT', 'Continuously Variable Transmission')], default='AUTOMATIC', max_length=10)),
-                ('car_make', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='djangoapp.carmake')),
+                (
+                    'type',
+                    models.CharField(
+                        choices=[
+                            ('SEDAN', 'Sedan'),
+                            ('COUPE', 'Coupe'),
+                            ('HATCHBACK', 'Hatchback'),
+                            ('SUV', 'SUV'),
+                            ('CROSSOVER', 'Crossover'),
+                            ('CONVERTIBLE', 'Convertible'),
+                            ('WAGON', 'Wagon'),
+                            ('PICKUP', 'Pickup Truck'),
+                            ('MINIVAN', 'Minivan'),
+                            ('SPORTS_CAR', 'Sports Car'),
+                            ('LUXURY_CAR', 'Luxury Car'),
+                            ('ELECTRIC', 'Electric Vehicle'),
+                            ('HYBRID', 'Hybrid Vehicle'),
+                            ('COMPACT', 'Compact Car')
+                        ],
+                        default='SUV',
+                        max_length=12
+                    )
+                ),
+                (
+                    'year',
+                    models.IntegerField(
+                        default=2023,
+                        validators=[
+                            django.core.validators.MaxValueValidator(2023),
+                            django.core.validators.MinValueValidator(2015)
+                        ]
+                    )
+                ),
+                (
+                    'transmission',
+                    models.CharField(
+                        choices=[
+                            ('MANUAL', 'Manual'),
+                            ('AUTOMATIC', 'Automatic'),
+                            ('CVT', 'Continuously Variable Transmission')
+                        ],
+                        default='AUTOMATIC',
+                        max_length=10
+                    )
+                ),
+                (
+                    'car_make',
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to='djangoapp.carmake'
+                    )
+                ),
             ],
         ),
     ]
